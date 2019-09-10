25def5e61c139299654d7c32fe5b1ee481084a6e
=mohamed Beshri Amer <mohamed.perry351@gmail.com>
2019-09-10 19:26:28 +0200

    add mobile nav in all pages

--- shared.js
+++ shared.js
@@ -2,21 +2,58 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector('.modal');
 var buttons = document.querySelectorAll('.plans .btn-plan');
 var closeButton = document.querySelector(".closed-button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobilenav = document.querySelector(".mobile-nav");
 
 
+if(backdrop){
+  backdrop.addEventListener("click",function(){
+    let opensModel = document.querySelectorAll(".open-model");
+    for (let index = 0; index < opensModel.length; index++) {
+      opensModel[index].classList.remove("open-model");
+      
+    } 
 
-for(var i=0;i<buttons.length;i++){
-    buttons[i].addEventListener("click",function(){
-      backdrop.style.display ='block';
-      modal.style.display='block';
-    });
+  });
 }
 
-closeButton.addEventListener("click",function(){
+if(buttons)
+{
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",open);
+}
+}
+if(closeButton)
+{
 
-    backdrop.style.display ='none';
-    modal.style.display='none';
+closeButton.addEventListener("click",close);
+}
+
+if(toggleButton){
+toggleButton.addEventListener("click",function(){
+  mobilenav.classList.add("open-model");
+  backdrop.classList.add("open-model");
 });
+}
+
+
+function open(){
+  if(backdrop){
+  backdrop.classList.add("open-model");
+  }
+  if(modal){
+  modal.classList.add("open-model");
+  }
+}
+function close(){
+  if(backdrop){
+  
+  backdrop.classList.remove("open-model");
+  }
+  if(modal){
+  modal.classList.remove("open-model");
+  }
+}
 
 
 
