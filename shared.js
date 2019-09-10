a4c85165da830ab9c1ac3fca471a136b3f5381a6
=mohamed Beshri Amer <mohamed.perry351@gmail.com>
2019-09-10 12:01:52 +0200

    udemy task

--- shared.js
+++ shared.js
@@ -3,9 +3,7 @@ var modal = document.querySelector('.modal');
 var buttons = document.querySelectorAll('.plans .btn-plan');
 var closeButton = document.querySelector(".closed-button");
 
-console.dir(backdrop);
-console.dir(buttons);
-console.dir(closeButton);
+
 
 for(var i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",function(){
@@ -14,6 +12,11 @@ for(var i=0;i<buttons.length;i++){
     });
 }
 
+closeButton.addEventListener("click",function(){
+
+    backdrop.style.display ='none';
+    modal.style.display='none';
+});
 
 
 
