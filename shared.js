38abd9955c18069c7ff9a9381e8176413d8b08f8
=mohamed Beshri Amer <mohamed.perry351@gmail.com>
2019-09-10 11:29:39 +0200

    Add Code to shared.js File

--- shared.js
+++ shared.js
@@ -1,6 +1,13 @@
 var backdrop = document.querySelector(".backdrop");
-var buttons = document.querySelector('.plans .btn-plan');
+var modal = document.querySelector('.modal');
+var buttons = document.querySelectorAll('.plans .btn-plan');
 
 console.dir(backdrop);
 console.dir(buttons);
+console.dir(modal);
 
+for(var i=0;i<buttons.length;i++){
+    buttons[i].addEventListener("click",function(){
+     
+    });
+}
