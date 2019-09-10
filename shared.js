bf48804ccae37ee10d43c84e4f52afeae2228089
=mohamed Beshri Amer <mohamed.perry351@gmail.com>
2019-09-10 11:38:47 +0200

    please add closed button

--- shared.js
+++ shared.js
@@ -1,13 +1,19 @@
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector('.modal');
 var buttons = document.querySelectorAll('.plans .btn-plan');
+var closeButton = document.querySelector(".closed-button");
 
 console.dir(backdrop);
 console.dir(buttons);
-console.dir(modal);
+console.dir(closeButton);
 
 for(var i=0;i<buttons.length;i++){
     buttons[i].addEventListener("click",function(){
-     
+      backdrop.style.display ='block';
+      modal.style.display='block';
     });
 }
+
+
+
+
